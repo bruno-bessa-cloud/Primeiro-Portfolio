5b14a0e9d74aac6a9eb5922910499b5c16a9e597
bruno-bessa-cloud <brunobomabessa15@gmail.com>
2025-07-30 12:38:38 +0100

    Add files via upload

---
+++ JavaScript/Aula1/Exercicios de Javas/array01.js
@@ -0,0 +1,16 @@
+let cidades = ["Brasilia", "Joao_Pinheiro", "Porto"];
+console.log(cidades);
+
+
+console.log( cidades[0]);
+console.log( cidades[1]);
+
+cidades[1] = "Lisboa";
+console.log(cidades);
+
+cidades.push("Faro");
+console.log(cidades);
+
+console.log(cidades.length);
+console.log(cidades.pop());
+console.log(cidades);
---
+++ JavaScript/Aula1/Exercicios de Javas/ex01.js
@@ -0,0 +1,35 @@
+let nome = "Bruno";
+console.log(nome);
+
+let idade = 24;
+console.log(idade);
+
+const PAIS = "Brasil";
+console.log(PAIS);
+
+let x = 1;
+let y = 2;
+let soma = x + y;
+console.log(soma);
+
+let frase = "Estou a aprender JavaScript";
+console.log(frase);
+
+let frasent = "Olá, meu nome é \n " +  "Bruno e tenho " + " \t 24 anos." + " e sou amigo do M\niguel";
+console.log(frasent);
+
+let Idade = 24;
+Idade++;
+console.log(Idade);
+
+let xy = 10;
+let yy = 2;
+let sub = xy - yy;
+console.log(sub);
+
+let base = 10;
+let altura = 5;
+let resultado = 10 * 5;
+console.log(resultado);
+
+
